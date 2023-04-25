f747f88d0fb16b0cfda8cfe9152ff56bbca133bf
Amune Israel <amuneisrael224@gmail.com>
2023-04-25 16:15:16 +0100

    made chanages

--- src/api/image.routes.ts
+++ src/api/image.routes.ts
@@ -1,8 +1,7 @@
 import express from 'express'
-import { uploadImage } from './image.service'
 import { deleteImageHandler, getAllImageHandler, getImageHandler, imageUplaodHandler } from './image.controller'
-import upload from '../utils/multersetup'
-import { Request,Response } from 'express'
+
+
 
 
 
@@ -12,6 +11,6 @@ const route =  express.Router()
 route.post('/v1/img-upload',imageUplaodHandler)
 route.get('/v1/get-image/:fileName',getImageHandler)
 route.get('/v1/get-images/',getAllImageHandler)
-route.delete('/v1/delete-iamge/:fileName',deleteImageHandler)
+route.delete('/v1/delete-image/:fileName',deleteImageHandler)
 
 export default route
